36a2339d94bce8ff9c8bb6677affc1dccec78b87
ezequielh <>
2023-02-26 14:24:40 -0800

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Kubernetes get $$$$$ $ $ 4 $
         </a>
       </header>
     </div>
